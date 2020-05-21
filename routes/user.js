23a57f2bbf5e10e98191d5a22b6037a46f7c9bd3
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-05-20 23:19:48 -0400

    Created the basic structure for user routes

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,31 @@ function asyncHandler(callback) {
     }
 } 
 //Send a GET request to /api/users to READ(view) currently authenticated user
+router.get('/users', asyncHandler(async(req, res) => {
+    const user = await User.findByPk();
+}));
 
 //Send a POST request to /api/users to CREATE a user 
+router.post('/users', asyncHandler(async(req, res) => {
+    let user;
+    try {
+        //if all the required fields have been submitted, redirect to home & set HTTP status code to 201
+        user = await User.create(req.body);
+        res.status(201).location('/api/users').end();
+
+    } catch (error) {
+        //checking the errors 
+        if(error.name === "SequelizeValidationError") {
+            const errors = error.errors.map(err => {return {
+                attribute: err.path,
+                message: err.message
+            }});
+            //display simplified error messages and set HTTP status code to 400 
+            res.status(400).json(errors)
+
+         //error caught in the asynchandler's catch block     
+        } else {throw error}
+    }
+}));
 
 module.exports = router;
